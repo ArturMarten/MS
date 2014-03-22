fa4c0aeebb9e7ca0a97fe75c43646e54dba71fd5
ArturMarten <arturmarten@hot.ee>
2014-03-22 15:13:01 +0200

    facebook login test

--- app/controllers/ApplicationController.java
+++ app/controllers/ApplicationController.java
@@ -3,9 +3,14 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.login;
+import views.html.facebooklogin;
 
 public class ApplicationController extends Controller {
 	public static Result login() {
 		return ok(login.render());
+	}
+	
+	public static Result facebooklogin() {
+		return ok(facebooklogin.render());
 	}
 }
