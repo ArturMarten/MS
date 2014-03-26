204bf879e10d8af7039174ea05dc37fc66e4a784
ArturMarten <arturmarten@hot.ee>
2014-03-26 14:03:40 +0200

    login fix

--- app/controllers/ApplicationController.java
+++ app/controllers/ApplicationController.java
@@ -7,20 +7,7 @@ import play.data.Form;
 import static play.data.Form.*;
 
 public class ApplicationController extends Controller {
-    public static Result login() {
-        return ok(
-            login.render(form(Login.class))
-        );
-    }
-
-	public static class Login {
-		public String email;
-		public String password;
-
-	}
-
-	public static Result authenticate() {
-		Form<Login> loginForm = form(Login.class).bindFromRequest();
-		return ok();
-	}
+		public static Result login() {
+			return ok(login.render());
+		}
 }
