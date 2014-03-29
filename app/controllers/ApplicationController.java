bb52309f783a645969bac6eb71d7b0c458ffe63f
ArturMarten <arturmarten@hot.ee>
2014-03-29 22:26:17 +0200

    register/login forms, html/css fix

--- app/controllers/ApplicationController.java
+++ app/controllers/ApplicationController.java
@@ -1,13 +1,70 @@
 package controllers;
 
+import com.avaje.ebean.Ebean;
+
+import models.Users;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.login;
+import views.html.register;
 import play.data.Form;
 import static play.data.Form.*;
 
 public class ApplicationController extends Controller {
-		public static Result login() {
-			return ok(login.render());
+	public static Result login() {
+		return ok(
+				login.render(form(Login.class))
+		);
+	}
+	
+	public static class Login {
+		public String email;
+		public String password;
+		
+		public String validate() {
+		    if (Users.authenticate(email, password) == null) {
+		      return "Invalid user or password";
+		    }
+		    return null;
 		}
+	}
+	
+	public static Result authenticate() {
+		Form<Login> loginForm = form(Login.class).bindFromRequest();
+		if (loginForm.hasErrors()) {
+	        return badRequest(login.render(loginForm));
+	    } else {
+	        session().clear();
+	        session("email", loginForm.get().email);
+	        return redirect(routes.MainController.maineditor("main_new"));
+	    }
+	}
+	
+	public static class Register {
+		public String email;
+		public String password;
+	}
+	
+	public static Result register() {
+    	Users user = null;
+    	try{
+    		user = Users.find.byId(session().get("email"));
+    	} catch(Exception e){}
+        return ok(register.render( 
+            form(Register.class)
+        )); 
+    }
+	
+	public static Result registerForm() {
+		Form<Register> registerForm = form(Register.class).bindFromRequest();
+		if (registerForm.hasErrors()) {
+			return badRequest(register.render(registerForm));
+		} 
+		else {
+			Users user = new Users(registerForm.get().email,registerForm.get().password);
+			Ebean.save(user);
+			session().put("email", registerForm.get().email);
+			return redirect(routes.MainController.maineditor("main_new"));
+		}
+	}
 }
--- app/models/Users.java
+++ app/models/Users.java
@@ -7,10 +7,19 @@ import play.db.ebean.Model;
 
 @Entity
 public class Users extends Model{
-	@Id
-	public int id;
-	
+	@Id	
 	public String email;
 	
 	public String password;
+	
+    public Users(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
+
+	public static Finder<String,Users> find = new Finder<String,Users>( String.class, Users.class); 
+	
+    public static Users authenticate(String email, String password) {
+        return find.where().eq("email", email).eq("password", password).findUnique();
+    }
 }
