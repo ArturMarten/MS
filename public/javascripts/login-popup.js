795ee557626614636eba53334b881172e506aa62
SixtenLeemets <suix@hot.ee>
2014-04-16 16:00:11 +0300

    popup focus on email input, editor's css to main

--- public/javascripts/login-popup.js
+++ public/javascripts/login-popup.js
@@ -2,10 +2,10 @@ window.onload = function() {
 	document.getElementById("logisisse").href = "#";
 	return false;
 };
-
 $(document).ready(function() {
     $(".popup_button").click(function() {
 		$(".popup").show();
+		document.getElementById("emailInput").focus();
 		$(".sulge_popup").click(function() {
 			$(".popup").hide();
 		});
