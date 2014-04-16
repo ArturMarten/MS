4ecd5e6a953f27317049e32476e1c9501e33df28
SixtenLeemets <suix@hot.ee>
2014-04-16 15:00:01 +0300

    validation working

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -1,25 +1,3 @@
-/*window.onload = function() {
-     initialiseStateFromURL();
-     //setInterval(initialiseStateFromURL, 1000);
-   }*/
-/*
-   var recentHash = "";
-   function pollHash() {
-  
-     if (window.location.hash==recentHash) {
-       return; // Nothing's changed since last polled.
-     }
-     recentHash = window.location.hash;
-  
-     // URL has changed, update the UI accordingly.
-     openTab(initialTab);
-  
-   }
-  
-function initialiseStateFromURL() {
- var initialTab = window.location.hash;
- openTab(initialTab);
-}*/
 $("#wrap a").click(function(){
 	window.location.hash = "#article";
 	$("#wrap").load(this.href + " #articlewrap");
