a9dd48a5cfa21719fe416225bbfc26e75def5aab
SixtenLeemets <suix@hot.ee>
2014-04-23 13:20:26 +0300

    Natuke offline mode'i

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -1,15 +1,30 @@
 $("#wrap a").click(function(){
-	window.location.hash = "#article";
-	$("#wrap").load(this.href + " #articlewrap");
+	window.location.hash = "article"+this.href.substring(this.href.lastIndexOf("/")+1,this.href.length);
+	$("#wrap").load(this.href + " #wrap");
 	return false
 })
 $("#sorteerimine a").click(function(){
-	window.location.hash = "#sort";
+	window.location.hash = this.href.substring(this.href.lastIndexOf("/"),this.href.length);
 	$("#wrap").load(this.href + " #wrap");
 	return false
 })
 $("#teemad a").click(function(){
-	window.location.hash = "#teemad";
+	window.location.hash = this.href.substring(this.href.lastIndexOf("/"),this.href.length);
 	$("#wrap").load(this.href + " #wrap");
 	return false
 })
+/*
+var recentHash = "";
+var checkHash = function() {
+  var hash = document.location.hash;
+  if (hash) {
+	hash = hash.substr(1);
+	if (hash == recentHash) {
+	  return;
+	}
+	recentHash = hash;
+	loadPage(hash);
+  }
+}
+setInterval(checkHash, 1000);
+*/
