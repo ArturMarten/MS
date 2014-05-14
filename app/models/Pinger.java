fd83cf3cfe1c0b104ca652134689632c1d612023
ArturMarten <arturmarten@hot.ee>
2014-05-14 13:01:34 +0300

    Time fix

--- app/models/Pinger.java
+++ app/models/Pinger.java
@@ -3,6 +3,7 @@ package models;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
+import java.util.TimeZone;
 
 import play.mvc.WebSocket;
 import akka.actor.UntypedActor;
@@ -21,7 +22,9 @@ public class Pinger extends UntypedActor {
 		if (message.equals("Tick")) {
 			Locale locale = new Locale("et", "ET");
 			SimpleDateFormat sdf = new SimpleDateFormat("EEEE dd.MMMM yyyy HH:mm:ss",locale);
+			
 			Calendar cal = Calendar.getInstance();
+			cal.setTimeZone(TimeZone.getTimeZone("Estonia/Tallinn"));
 			out.write(sdf.format(cal.getTime()));
 		} else {
 			unhandled(message);
